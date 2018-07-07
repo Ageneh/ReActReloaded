129d017215e0f72ab4896f9b03cea5bd90a06809
Henock Arega <Henock.Arega@student.hs-rm.de>
2018-07-07 18:51:28 +0200

    created Continuous gameplay with Scene

--- src/scenes/gamemodes/GameScene.java
+++ src/scenes/gamemodes/GameScene.java
@@ -34,16 +34,14 @@ import java.util.Observer;
  * @author Henock Arega
  * @project ReActReloaded
  */
-public abstract class GameScene extends ObservableScene implements Observer, Close {
+public abstract class GameScene<T extends GameMode> extends ObservableScene implements Observer, Close {
     
-    private GameBackground background;
-    private Label points;
-    private Label username;
-    private Label multi;
-    private Label round;
-    private User user;
-    private HBox top;
-    private String fxmlPath;
+    protected final Labels LABEL_STYLE = Labels.M_SMALL;
+    protected final T game;
+    protected Label points;
+    protected Label username;
+    protected Label multi;
+    protected Label round;
     protected HBox buttons;
     protected ObservableList<AnswerButton> buttonsList;
     protected ReButton start;
@@ -51,9 +49,12 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
     protected SimpleBooleanProperty answered;
     protected PauseTransition pauseTransition;
     protected FadeTransition fadeTransition;
-    protected final GameMode game;
+    private GameBackground background;
+    private User user;
+    protected HBox top;
+    private String fxmlPath;
     
-    GameScene(String fxmlPath, GameMode game, Observer o) {
+    GameScene(String fxmlPath, T game, Observer o) {
         super();
         this.game = game;
         this.game.addObserver(o);
@@ -62,12 +63,11 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
         this.fxmlPath = fxmlPath;
         this.background = new GameBackground();
         
-        Labels label = Labels.M_SMALL;
-        this.points = label.getLabel(0);
-        this.username = label.getLabel("Player");
-        this.multi = label.getLabel("1x");
-        this.round = label.getLabel("1");
-    
+        this.points = LABEL_STYLE.getLabel(0);
+        this.username = LABEL_STYLE.getLabel("Player");
+        this.multi = LABEL_STYLE.getLabel("1x");
+        this.round = LABEL_STYLE.getLabel("1");
+        
         this.started = new SimpleBooleanProperty(false);
         this.started.addListener(((observable, oldValue, newValue) -> {
             if (oldValue) this.started.set(oldValue);
@@ -75,7 +75,7 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
                 this.setStartBG();
             }
         }));
-    
+        
         this.answered = new SimpleBooleanProperty(false);
         this.answered.addListener((observable, oldValue, newValue) -> {
             if (newValue) {
@@ -95,6 +95,69 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
         getRoot().getChildren().add(this.background.root);
     }
     
+    public BorderPane getBackground() {
+        return background.root;
+    }
+    
+    public String getFxmlPath() {
+        return fxmlPath;
+    }
+    
+    public void ready() {
+        this.setAnswers(game.getAnswers());
+    }
+    
+    public void setUser(String username) {
+        this.game.setUser(username);
+    }
+    
+    public void start() {
+        this.game.start();
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(300), this.buttons);
+        fadeTransition.setByValue(0.1);
+        fadeTransition.setFromValue(this.buttons.getOpacity());
+        fadeTransition.setToValue(1);
+        fadeTransition.setCycleCount(1);
+        fadeTransition.play();
+    }
+    
+    protected void addLayer(Node node) {
+        this.addLayers(node);
+    }
+    
+    protected void addLayers(Node... node) {
+        if (node == null) return;
+        for (Node n : node) {
+            if (n == null) continue;
+            this.background.bottom.getChildren().add(n);
+        }
+    }
+    
+    protected void setAnswers(Song[] songs) {
+        ArrayList<AnswerButton> btns = new ArrayList<>();
+        System.out.println(songs.length);
+        int i = 0;
+        for (Song song : songs) {
+            AnswerButton a = buttonsList.get(i++);
+            a.setText(song.getTitle());
+            a.setSong(song);
+            a.setOnAction(event -> {
+                if (this.started.get()) {
+                    boolean correct = game.answer(song);
+                    a.setColor(correct);
+                    setChanged();
+                    if (correct) {
+                        notifyObservers(isGame.Action.ANSWER_CORRECT);
+                    } else {
+                        notifyObservers(isGame.Action.ANSWER_INCORRECT);
+                    }
+                }
+            });
+            btns.add(a);
+        }
+        this.buttons.getChildren().setAll(btns);
+    }
+    
     protected void setStartBG() {
         if (! this.started.get() && ! this.answered.get()) {
             start.setBackground(ElementBackgroundCreator.createBackgroundImg("res/icons/icon_play.png"));
@@ -103,7 +166,7 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
         }
     }
     
-    private void init(){
+    private void init() {
         this.buttonsList = FXCollections.observableList(new ArrayList<>());
         buttons = new HBox();
         buttons.getChildren().setAll(this.buttonsList);
@@ -116,7 +179,7 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
         buttons.setPrefHeight(80);
         
         BorderPane root = new BorderPane();
-    
+        
         this.start = new ReButton("");
         this.start.setBackground(ElementBackgroundCreator.createBackgroundImg("res/icons/icon_play.png"));
         root.setCenter(start);
@@ -136,64 +199,15 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
                 }
             }
         });
-    
-    
+        
+        
         for (int i = 0; i < game.MAX_ANSWERCOUNT; i++) {
             buttonsList.add(new AnswerButton());
         }
         root.setBottom(buttons);
-    
+        
         this.background.root.setLeft(new BackButton("Zurück"));
         addLayer(root);
-    }
-    
-    public void start(){
-        this.game.start();
-        FadeTransition fadeTransition = new FadeTransition(Duration.millis(300), this.buttons);
-        fadeTransition.setByValue(0.1);
-        fadeTransition.setFromValue(this.buttons.getOpacity());
-        fadeTransition.setToValue(1);
-        fadeTransition.setCycleCount(1);
-        fadeTransition.play();
-    }
-    
-    public BorderPane getBackground() {
-        return background.root;
-    }
-    
-    public String getFxmlPath() {
-        return fxmlPath;
-    }
-    
-    protected void addLayer(Node node) {
-        this.addLayers(node);
-    }
-    
-    protected void addLayers(Node... node) {
-        if (node == null) return;
-        for (Node n : node) {
-            if (n == null) continue;
-            this.background.bottom.getChildren().add(n);
-        }
-    }
-    
-    private void setUser(User user) {
-        if (user != null) return;
-        
-        this.user = user;
-        
-        this.points.setText("0");
-        this.username.setText(this.user.getName());
-        this.multi.setText("1x");
-        this.round.setText("1");
-    }
-    
-    public void setUser(String username) {
-        this.game.setUser(username);
-    }
-    
-    public void ready() {
-        this.setAnswers(game.getAnswers());
     }
     
     private void setAnswers(ArrayList<Song> songs) {
@@ -218,39 +232,25 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
         });
     }
     
-    protected void setAnswers(Song[] songs) {
-        ArrayList<AnswerButton> btns = new ArrayList<>();
-        System.out.println(songs.length);
-        int i = 0;
-        for (Song song : songs) {
-            AnswerButton a = buttonsList.get(i++);
-            a.setText(song.getTitle());
-            a.setSong(song);
-            a.setOnAction(event -> {
-                if (this.started.get()) {
-                    boolean correct = game.answer(song);
-                    a.setColor(correct);
-                    if (! correct) {
-                        Alert alert = new Alert(Alert.AlertType.ERROR);
-                        alert.setTitle("Game Over");
-                        alert.setContentText("You reached " + game.getPoints() + " points.");
-                        alert.showAndWait();
-                    }
-                }
-            });
-            btns.add(a);
-        }
-        this.buttons.getChildren().setAll(btns);
+    private void setUser(User user) {
+        if (user != null) return;
+        
+        this.user = user;
+        
+        this.points.setText("0");
+        this.username.setText(this.user.getName());
+        this.multi.setText("1x");
+        this.round.setText("1");
     }
     
     @Override
     public void update(Observable o, Object arg) {
         ANSI.RED.println("__UPDATE__");
-    
+        
         try {
             if (arg instanceof GameMode.Mode) {
                 GameMode.Mode mode = (GameMode.Mode) arg;
-                switch (mode){
+                switch (mode) {
                     case NORMAL:
                         break;
                     case TIMED:
@@ -284,8 +284,11 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
                         this.answered.set(false);
                         break;
                     case ANSWER_INCORRECT:
-                        // todo
                         this.answered.set(true);
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Game Over");
+                        alert.setContentText("You reached " + game.getPoints() + " points.");
+                        alert.showAndWait();
                         break;
                     case ANSWER:
                         break;
@@ -293,7 +296,7 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
             }
         } catch (NullPointerException e) {
         }
-    
+        
         if (arg instanceof isGame.Action) {
             isGame.Action action = (isGame.Action) arg;
             switch (action) {
@@ -309,11 +312,11 @@ public abstract class GameScene extends ObservableScene implements Observer, Clo
     }
     
     private class GameBackground {
-    
+        
         private BorderPane root;
         private StackPane bottom;
         private Scene scene;
-    
+        
         private GameBackground() {
             this.bottom = new StackPane();
             this.root = new BorderPane();
